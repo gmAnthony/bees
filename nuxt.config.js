32056d728fb78182c54eaa73e69b405f7dc881a6
gmAnthony <gm.anthony@gmail.com>
2020-08-07 08:37:38 -0500

    og-image

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { 'property': 'og.image',
-        'content': 'assets/bee.png'}
+      { hid: 'og:image', name: 'og:image', content: 'assets/bee.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'assets/favicon.ico' }
