8562900825a2b790307810f398b79f02673cb9bb
gmAnthony <gm.anthony@gmail.com>
2020-08-07 08:07:01 -0500

    add og-image

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { 'property': 'og.image',
+        'content': 'assets/bee.png'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'assets/favicon.ico' }
